a09aaaf78650bc9eabeba1c172c80c5c824052d3
Biswadeep Sahoo <BSahoo@walmartlabs.com>
2019-05-19 00:01:10 +0530

    Maven Basics

---
+++ src/test/java/com/qa/tests/DemoTest1.java
@@ -0,0 +1,47 @@
+package com.qa.tests;
+
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest1 {
+
+	
+	@Test
+	
+	public void Multiply() {
+		System.out.println("MULTIPLY");
+		int a=10;
+		int b = 20;
+		AssertJUnit.assertEquals(200, b*a);
+	}
+	
+	@Test
+	
+	public void Div() {
+		System.out.println("DIV");
+		int a=10;
+		int b = 20;
+		AssertJUnit.assertEquals(2, b/a);
+	}
+	
+	@Test
+	
+	public void Sum() {
+		System.out.println("SUM");
+		int a=10;
+		int b = 20;
+		AssertJUnit.assertEquals(30, a+b);
+	}
+	
+	@Test
+	
+	public void Sub() {
+		System.out.println("SUB");
+		int a=10;
+		int b = 20;
+		AssertJUnit.assertEquals(10, b-a);
+	}
+	
+}
